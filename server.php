6e662f6e8e1120f21c2bcf7278eb926808886c64
AverageHippo <32309984+AverageHippo@users.noreply.github.com>
2018-06-28 11:06:07 -0600

    Shoot, my laptop is ded.. and by dead i mean
    That the monitor doesn't work and its a 400$ laptop.... I guess I will get my wish and buy a workhorse that'll last me a few years

--- server.php
+++ server.php
@@ -2,19 +2,46 @@
 <html>
 <body>
     <?php                                 
-        $servername="jdbc:mysql://localhost:3306/deliveryorders"
-        $username = "dewskies";
+        $username="root";
         $password = "N3rf1r311A";
         
         //create connection
-        $conn = new mysqli($servername, $username, $password);
+        $conn = new PDO("mysql:host=localhost:3306;dbname=deliveryorders", $username, $password);
         
         //Check connection
         if ($conn->connect_error)
         {
             die("Connection failed: " . $conn->connect_error);
         }
-        echo "Connected successfully";
+        echo "Connected successfully \n";
+        $name = $_POST["name"];
+        $water = $_POST["water"];
+        $location = $_POST["location"];
+        $quantity = $_POST["number"];
+        if (date("l") == "Monday")
+        {
+            echo "monday";
+        }
+        else if (date("l") == "Tuesday")
+        {
+            echo "tuesday";
+        }
+        else if (date("l") == "Wednesday")
+        {
+            echo "wednesday";
+        }
+        else if (date("l") == "Thursday")
+        {
+            echo "thursday";
+        }
+        else if (date("l") == "Friday")
+        {
+            echo "friday";
+        }
+        else
+        {
+            echo "this date is ineligible for delivery";
+        }
     ?>
 </body>
 </html>
